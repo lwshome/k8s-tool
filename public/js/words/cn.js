3a5e4c22eeabdc84bf5e11b1d6c123abef3515f3
wensheng li <lws_home@hotmail.com>
2023-05-02 21:45:46 -0400

    main: d

--- public/js/words/cn.js
+++ public/js/words/cn.js
@@ -1,77 +1,83 @@
 const _k8sMessage={
   _common:{
-     _namespace:"命名空间",
-      _name:"名称",
-      _value:"值",
-      _repeat:"重复",
-      _message:"信息",
-      _ok:"好",
-      _waiting:"等待中 ...",
-      _question:"问题",
-      _empty:"空值"
-   },
-   _info:{
-     _downloadInBG:"文件/文件夹已在服务器端下载。",
-     _askPort:"请设置转发端口:",
-     _confirmStopForwarding:"你想停止转发来自:",
-     _confirmDelete:"你想删除该项目吗？",
-     _askFileName:"请问文件名是什么？",
-     _importFileError:"导入文件时出错:{0}",
-     _noMoreAsk:"不再询问",
-     _askSearchFile:"需要搜索的文件"
-   },
-   _method:{
-     _search:"搜索",
-     _confirm:"确认",
-     _filter:"过滤器",
-     _yes:"是",
-     cmd:"执行远程命令",
-     link:"在浏览器中打开链接",
-     api:"API 测试",
-     forward:"转发",
-     search:"搜索文件/文件夹",
-     refresh:"刷新",
-     log:"监控记录",
-     star:"青睐",
-     _refresh:"刷新",
-     _download:"下载",
-     _close:"关闭",
-     _copy:"复制物品名称",
-     download:"下载",
-     delete:"删除",
-     _save:"保存",
-     _cancel:"取消",
-     _close:"关闭",
-     _clean:"干净",
-     _addItem:"添加项目",
-     "add-folder":"添加文件夹",
-     "add-file":"添加文件",
-     "delete-pod":"删除（重建）pod"
-   },
-   _main:{
-     _tabs:{
-       _namespace:"命名空间",
-       _pods:"豆荚",
-       _services:"服务",
-       _deployments:"部署"
-     }
-   },
-   _setting:{
-     _title:"设置 ...",
-     cmd:"远程命令设置",
-     link:"Pod 链接设置",
-     _list:"列表:"
-   },
-   _log:{
-     _title:"日志设置",
-     _highlightTitle:"突出显示",
-     _regexValue:"过滤正则表达式",
-     _style:"高光样式 (css)",
-     _example:"例子",
-     formatJSON:"自动格式化 JSON 数据",
-     formatXML:"自动格式化 XML 数据",
-     groupMerge:"按过滤器分组日志",
-     _autoMerge:"自动合并日志（合并重复内容）",
-     _cleanDynamicRegex:"清理动态字符的正则表达式"
-   }
+    _namespace:"命名空间",
+    _name:"名称",
+    _value:"值",
+    _repeat:"重复",
+    _message:"信息",
+    _ok:"好",
+    _waiting:"等待中 ...",
+    _question:"问题",
+    _empty:"空值"
+  },
+  _info:{
+    _title:"Boozang K8s-工具",
+    _downloadInBG:"文件/文件夹已在服务器端下载。",
+    _askPort:"请设置转发端口:",
+    _confirmStopForwarding:"你想停止转发来自:",
+    _confirmDelete:"你想删除该项目吗？",
+    _askFileName:"请问文件名是什么？",
+    _importFileError:"导入文件时出错:{0}",
+    _noMoreAsk:"不再询问",
+    _askSearchFile:"需要搜索的文件"
+  },
+  _method:{
+    _execute:"执行",
+    _nsFilter:"命名空间过滤器",
+    _itemFilter:"Pod/Service 过滤器",
+    _search:"搜索",
+    _confirm:"确认",
+    _filter:"过滤器",
+    _yes:"是",
+    cmd:"执行远程命令",
+    link:"在浏览器中打开链接",
+    api:"API 测试",
+    forward:"转发",
+    search:"搜索文件/文件夹",
+    refresh:"刷新",
+    log:"监控记录",
+    star:"青睐",
+    _refresh:"刷新",
+    _download:"下载",
+    _close:"关闭",
+    _copy:"复制物品名称",
+    download:"下载",
+    delete:"删除",
+    _save:"保存",
+    _cancel:"取消",
+    _close:"关闭",
+    _clean:"干净",
+    _addItem:"添加项目",
+    _try:"试测",
+    "add-folder":"添加文件夹",
+    "add-file":"添加文件",
+    "delete-pod":"删除（重建）pod"
+  },
+  _main:{
+    _tabs:{
+      _namespace:"命名空间",
+      _pods:"豆荚",
+      _services:"服务",
+      _deployments:"部署"
+    }
+  },
+  _setting:{
+    _title:"设置 ...",
+    cmd:"远程命令设置",
+    link:"Pod 链接设置",
+    api:"API 设置",
+    _list:"列表:"
+  },
+  _log:{
+    _title:"日志设置",
+    _highlightTitle:"突出显示",
+    _regexValue:"过滤正则表达式",
+    _style:"高光样式 (css)",
+    _example:"例子",
+    formatJSON:"自动格式化 JSON 数据",
+    formatXML:"自动格式化 XML 数据",
+    groupMerge:"按过滤器分组日志",
+    autoMerge:"自动合并日志（合并重复内容）",
+    _cleanDynamicRegex:"清理动态字符的正则表达式"
+  }
 }
